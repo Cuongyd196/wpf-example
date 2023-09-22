c27989a2b7d5dd8b112407d2fd66e269dff72549
Cuongyd196 <cuongyd196@gmail.com>
2023-09-22 10:57:44 +0700

    #update ToolTip

---
+++ ToolTip/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToolTip
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToolTipsSimpleSample_Click(object sender, RoutedEventArgs e)
+        {
+            ToolTipsSimpleSample toolTipsSimpleSample = new ToolTipsSimpleSample();
+            toolTipsSimpleSample.Show();
+        }
+
+        private void btnToolTipsAdvancedSample_Click(object sender, RoutedEventArgs e)
+        {
+            ToolTipsAdvancedSample toolTipsAdvancedSample = new ToolTipsAdvancedSample();
+            toolTipsAdvancedSample.Show();
+        }
+    }
+}
